af00b5a7aa9afb36da49c7b5e520f64b2f297fe0
Tania <drob.tania@gmail.com>
2020-04-05 15:44:48 +0300

    4 players , problem with deleting a player

--- hw1.py
+++ hw1.py
@@ -1,20 +1,111 @@
 import random
+class Player(object):
+    def __init__(self, num, func):
+        self.number = num
+        self.choice = 'None'
+        self.resultNum = -9999
+        self.func = func
+        self.active = True
+
+    def show(self):
+        print("player num:", self.number, "choice:", self.choice, "ourNum:", self.resultNum)
+
+iterationCounter = 0
+T = 4
+
+def f1(X1,X2,T):
+    if (X1>130):
+        choice = 'S'
+    else:
+        choice = 'R'
+    return choice
+
+def f2(X1,X2,T):
+    if (X2>100):
+        choice = 'S'
+    else:
+        choice = 'R'
+    return choice
+
+def f3(X1,X2,T):
+    if (T>2):
+        choice = 'S'
+    else:
+        choice = 'R'
+    return choice
+
+def f4(X1,X2,T):
+    if (X2-45 <= 30):
+        choice = 'S'
+    else:
+        choice = 'R'
+    return choice
+
+
+Players = [Player(1, f1), Player(2, f2), Player(3, f3), Player(4, f4)]
+print ([player.active for player in Players])
 
 X1 = random.randint(0, 150)
+# TODO where to put X2
 X2 = random.randint(0, 150)
 
-print("x1:", X1, "X2:", X2)
 
 
-
-
-def falseIsOne(bool):
-    if bool == False:
-        print('1')
-    elif bool == True:
-        print('0')
+def calcResult(choice):
+    if choice == 'S':
+        ourNum = X1
     else:
-        print('none')
+        ourNum = random.randint(X2-45, X2+45)
+    return ourNum
+
+
+for player in Players:
+    print(player.resultNum)
+
+
+
+for player in Players:
+    print(player.resultNum)
+
+
+
+#while (iterationCounter < 100):
+while (T > 0):
+    ourNum = 0
+
+    print("new round----------------------------------------------------")
+    print("number of active players:", len(Players))
+
+    X1 = random.randint(0, 150)
+    #TODO move somewhere
+    X2 = random.randint(0, 150)
+
+    #for every player activate the descision function
+    for player in Players:
+        player.choice = player.func(X1, X2, T)
+
+    for player in Players:
+        player.resultNum = calcResult(player.choice)
+
+    for player in Players:
+        print(player.resultNum)
+
+    resultNums = (player.resultNum for player in Players)
+    print("min num : ")
+    minNum = min(resultNums)
+    print(minNum)
+
+    # array of results
+    newPlayers = filter(lambda player: player.resultNum > minNum, Players)
+
+    #Players = newPlayers
+
+    for player in Players:
+        print(player.resultNum)
+    #remove someone from Players
+    T=T-1
+
+
 
 
 
